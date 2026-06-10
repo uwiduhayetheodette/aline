a49ec90162bd43fbaf5ae2c8c897a3477bd383f2
uwiduhayetheodette <uwiduhayetheodette350@gmail.com>
2026-06-10 01:49:50 -0700

    Add files via upload

---
+++ erea.js
@@ -0,0 +1,19 @@
+function areaSquare(side) {
+    return side * side;
+}
+
+console.log(areaSquare(5)); 
+
+//area ractangulala
+function areaRectangle(length, width) {
+    return length * width;
+}
+
+console.log(areaRectangle(6, 4)); 
+
+//area cycle
+function areaCircle(radius) {
+    return Math.PI * radius * radius;
+}
+
+console.log(areaCircle(3)); 
